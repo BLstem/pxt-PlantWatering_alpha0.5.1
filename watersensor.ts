1964eed46aac8e33f8196c7b7e21be65ce58b153
DoraLC <34760377+DoraLC@users.noreply.github.com>
2018-07-30 17:44:21 +0800

    update

---
+++ watersensor.ts
@@ -0,0 +1,15 @@
+/**Developed by BL Contenets team.*/
+
+//%color=#01DF01 icon="\uf043" block="watersensor"
+
+namespace watersensor {
+
+    /**Read the analog output from the water sensor */
+    //%block="water sensor analog read pin %arg_pin"
+    //%blockId=watersensorreading
+    //%arg_pin.fieldEditor="gridpicker" arg_pin.fieldOptions.columns=5 arg_pin.defl=AnalogPin.P1
+    export function watersensorreading(arg_pin: AnalogPin): number {
+        let x = -1 * pins.analogReadPin(arg_pin)
+        return pins.map(x, -1023, 0, 0, 1023)
+    }
+}
